557f26128c324a85a8066a78e5cc0dc8c1967645
amoblin@mbp <amoblin@gmail.com>
2018-12-29 17:08:27 +0800

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const util = require('util')
+const path = require('path')
 
 const weekdayOfDay = function(day, weekday) {
     const delta = weekday - day.getDay()
@@ -26,21 +27,31 @@ const workdays = (startDay, endDay, config) => {
     const eSun = weekdayOfDay(eDate, 0)
 
     const timeDiff = Math.abs(sMon.getTime() - eMon.getTime()) / 1000
-    const weeks = Math.ceil(timeDiff / (3600 * 24 * 7))
+    const weeks = Math.ceil(timeDiff / (3600 * 24 * 7)) + 1
     const results = []
-    let Mon = sMon
+    let d = sMon
     for (let i=0; i < weeks; i++) {
-        const item = {}
-        item.date = Mon.toJSON().split("T")[0]
-        console.log(item)
+        const item = {
+            workdays: []
+        }
+        item.date = d.toJSON().split("T")[0]
+        const Sun = deltaDay(d, -1)
+        if (config.workdays.includes(date2str(Sun))) {
+            item.workdays.push(date2str(Sun))
+        }
+        for (let j=0; j < 5; j++) {
+            if (config.holidays.includes(date2str(d)) == false) {
+                item.workdays.push(date2str(d))
+            }
+            d.setDate(d.getDate() + 1)
+        }
+        if (config.workdays.includes(date2str(d))) {
+            item.workdays.push(date2str(d))
+        }
+        d.setDate(d.getDate() + 2)
+        results.push(item)
     }
-
-    /*
-    console.log(sDate)
-    console.log(sSunday)
-    console.log(sMonday)
-    console.log(sSatday)
-    */
+    return results
 }
 
 const data = require("../data/2018.json")
@@ -50,24 +61,43 @@ const config = {
     holidays: []
 }
 
+const date2str = (d) => {
+    return d.toJSON().split("T")[0]
+}
+
 for (const holiday of data.holidays) {
     const d = new Date(holiday.start)
-    config.holidays.push(d.toJSON().split("T")[0])
+    config.holidays.push(date2str(d))
     if (holiday.days && holiday.days > 1) {
         for(let i=1; i<holiday.days; i++) {
             //ts += 24 * 60 * 60
             d.setDate(d.getDate() + 1)
-            config.holidays.push(d.toJSON().split("T")[0])
+            config.holidays.push(date2str(d))
         }
     }
     if (holiday.workday) {
         for (const ds of holiday.workday) {
-            config.workdays.push(new Date(ds).toJSON().split("T")[0])
+            config.workdays.push(date2str(new Date(ds)))
         }
     }
 }
 
-//workdays("2018-08-03", "2018-12-29")
-//workdays("2018-08-04", "2018-12-30")
-workdays("2018-12-20", "2018-12-25", config)
-//workdays("2018-12-30", "2019-02-02")
+const args = process.argv.slice(2);
+const script_name  = path.basename(__filename);
+if (args.length < 1) {
+    console.log("Usage: ./%s [start date]", script_name);
+    console.log("       ./%s [start date] [end date]", script_name);
+    return;
+}
+
+const startStr = args[0]
+
+let endStr
+if (args.length < 2) {
+   endStr = date2str(new Date())
+} else {
+    endStr = args[1]
+}
+
+let results = workdays(startStr, endStr, config)
+console.log(results)
